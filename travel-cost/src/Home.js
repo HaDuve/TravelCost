ccd2c498b54b54d629edcc5e92780f927e62e705
HaDuve <hionoe@hotmail.de>
2022-07-07 14:49:43 +0800

    following tutorial 10 (#1)

--- travel-cost/src/Home.js
+++ travel-cost/src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [entries, setEntries] = useState([
+        { title: "pizza in germany", body: "01.01.2022", author: "Hannes", id: 1 },
+        { title: "rice and curry in sri lanka", body: "02.01.2022", author: "Hannes", id: 2 },
+        { title: "tempeh in bali", body: "03.01.2022", author: "Tina", id: 3 }
+    ]);
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            {entries.map((entry) => (
+                <div className="blog-preview" key={entry.id}>
+                    <h2> {entry.title} </h2>
+                    <p>Bought by {entry.author}</p>
+                </div>
+            )) }
         </div>
      );
 }
