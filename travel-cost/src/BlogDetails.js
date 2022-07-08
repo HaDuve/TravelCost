c0ba8987e04575d001c2e87d82d4332827d3c283
HaDuve <hionoe@hotmail.de>
2022-07-08 17:14:56 +0800

    added delete function tut31 (#1)

--- travel-cost/src/BlogDetails.js
+++ travel-cost/src/BlogDetails.js
@@ -1,10 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/entries/'+ id);
-
+    const history = useHistory();
+    
+    const handleClick = () => {
+        fetch("http://localhost:8000/entries/" + blog.id, { 
+            method: "DELETE",
+        }).then(() => {
+            history.push('/');
+        });
+    }
 
     return ( 
         <div className="blog-details">
@@ -15,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
