0f51666f17fd04233ccb0cf91f88738e753c3aaa
HaDuve <hionoe@hotmail.de>
2022-07-08 17:06:54 +0800

    creating new blogs tut30 (#1)

--- travel-cost/src/Create.js
+++ travel-cost/src/Create.js
@@ -1,7 +1,61 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('yoshi');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+        setIsPending(true);
+
+        fetch("http://localhost:8000/entries", { 
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("Created")
+            setIsPending(false);
+            history.push('/');
+        })
+    }
+
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
+            <form onSubmit={handleSubmit}>
+
+                <label htmlFor="">Blog title:</label>
+                <input
+                    type="text"
+                    value={title}
+                    required
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label htmlFor="">Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                    >
+                </textarea>
+
+                <label htmlFor="">Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                { !isPending && <button>Add Blog</button>}
+                { isPending && <button disabled>Adding Blog...</button>}
+            </form>
         </div>
      );
 }
